15ab3f13f69d0921af0be0c9769ddad18e639644
L. Chang <32004044+maketext@users.noreply.github.com>
2023-02-18 16:54:15 +0900

    머신비전 솔루션 데모 중 아두이노 소스코드 부분
    머신비전 솔루션 데모 중 아두이노 소스코드 부분

--- ArduinoSupport.ino
+++ ArduinoSupport.ino
@@ -1,8 +1,8 @@
 // Receive hardware trigger signals from equipment or test electronic switches and transmit RS232 signals to the Lighting Controller. The Software Part.
 // 장비 또는 테스트용 전자스위치로부터 하드웨어 트리거 신호를 받아 조명 컨트롤러로 RS232 신호 전송. 솔루션에 사용되는 소프트웨어 파트.
 
-const DATAIN_PIN = 21
-const DATAOUT_PIN = 3
+const DATAIN_PIN = 21 // Interrupt Support Pin base on MEGA 2560
+const DATAOUT_PIN = 3 // PWM Digital Pin
 
 volatile boolean current;
 
