d44f18307e1905deed2440948e0cf2c36df5bb3d
L. Chang <32004044+maketext@users.noreply.github.com>
2023-02-20 15:12:13 +0900

    도움말 수정
    도움말 수정

--- SignalGenerator.ino
+++ SignalGenerator.ino
@@ -1,5 +1,6 @@
-// 장비에서 나오는 신호를 가상으로 만든 디지털 펄스 신호 생성기 입니다.
+// 장비에서 나오는 신호를 가상으로 만든 디지털 펄스 신호 생성기 입니다. 비공식 아두이노 Mega 2560 보드의 10번 핀은 PWM을 지원합니다.
 // A digital pulse signal generator that virtualizes signals from the equipment.
+// The 10 pin on the unofficial Arduino Mega 2560 board supports PWM.
 
 const int TRIGGER_PIN = 10;
 
